3050c27b6c7242124ef70f5ff6a2845c8cbe8d5f
Filip Petkov <filip55555@abv.bg>
2023-02-07 11:13:51 +0200

    after linters beautifying

--- Software_metrics_exam_resit/math_function.py
+++ Software_metrics_exam_resit/math_function.py
@@ -1,12 +1,17 @@
-"""Exam Resit for Software Metrics - implementation of the math function math.isfinite()"""
+"""
+Exam Resit for Software Metrics
+implementation of the math function math.isfinite()
+"""
 import math
 
-def is_finite(input):
+
+def is_finite(input_arg):
     """Return True if x is neither an infinity nor a NaN, and False otherwise."""
-    if (input is None):
+    if input_arg is None:
         raise TypeError("No arguments were given to the method! Method requires a float or int argument!")
-    if not isinstance(input,(int,float)):
-        raise TypeError(f"Given argument is not valid. Arugment should be int or float! Your input: {type(input)}")
-    return math.isfinite(input)
+    if not isinstance(input_arg, (int, float)):
+        raise TypeError(f"Given argument is not valid. Arugment should be int or float! Your input: {type(input_arg)}")
+    return math.isfinite(input_arg)
+
 
 is_finite(5)
--- Software_metrics_exam_resit/test_cases.py
+++ Software_metrics_exam_resit/test_cases.py
@@ -1,4 +1,7 @@
-"""Exam Resit for Software Metrics - implementation of test cases for the math function math.isfinite()"""
+"""
+Exam Resit for Software Metrics
+implementation of test cases for the math function math.isfinite()
+"""
 import math
 import unittest
 from math_function import is_finite
@@ -8,13 +11,13 @@ class TestIsFiniteMethods(unittest.TestCase):
     """Test the is_finite method"""
     def test_right_result(self):
         """test if the result will be right with proper arguments"""
-        self.assertEqual(is_finite(2000),True)
-        self.assertEqual(is_finite(math.inf),False)
+        self.assertEqual(is_finite(2000), True)
+        self.assertEqual(is_finite(math.inf), False)
 
     def test_wrong_result(self):
         """test if the result will be wrong with proper arguments"""
-        self.assertNotEqual(is_finite(0.0),False)
-        self.assertNotEqual(is_finite(-math.inf),True)
+        self.assertNotEqual(is_finite(0.0), False)
+        self.assertNotEqual(is_finite(-math.inf), True)
 
     def test_without_arguments(self):
         """test without any given arguments to the method"""
@@ -23,19 +26,16 @@ class TestIsFiniteMethods(unittest.TestCase):
 
     def test_incorrect_arguments_string(self):
         """Test with invalid arguments, specifically with string"""
-        input_string="5"
+        input_string = "5"
         with self.assertRaises(TypeError):
             is_finite(input_string)
 
     def test_incorrect_arguments_array(self):
         """Test with invalid arguments, specifically with array"""
-        input_array=[5,2]
+        input_array = [5, 2]
         with self.assertRaises(TypeError):
             is_finite(input_array)
 
+
 if __name__ == "__main__":
     unittest.main(verbosity=2)
-
-
-
-
