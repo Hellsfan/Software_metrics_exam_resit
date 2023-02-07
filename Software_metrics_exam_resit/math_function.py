0f1093c3572880e9d0bafd185966e8ad728b4b17
Filip Petkov <filip55555@abv.bg>
2023-02-07 10:53:41 +0200

    implementation of the math function isfinite(), after implementing test cases.

--- Software_metrics_exam_resit/math_function.py
+++ Software_metrics_exam_resit/math_function.py
@@ -1,5 +1,12 @@
 """Exam Resit for Software Metrics - implementation of the math function math.isfinite()"""
 import math
 
-def is_finite():
-    pass;
+def is_finite(input):
+    """Return True if x is neither an infinity nor a NaN, and False otherwise."""
+    if (input is None):
+        raise TypeError("No arguments were given to the method! Method requires a float or int argument!")
+    if not isinstance(input,(int,float)):
+        raise TypeError(f"Given argument is not valid. Arugment should be int or float! Your input: {type(input)}")
+    return math.isfinite(input)
+
+is_finite(5)
